95c1241838d7d0fbb033baf5b913d3e3ad48ebf1
Phillip Smith <psmith586@yahoo.com>
2016-08-10 14:35:41 -0700

    script to rebuild the database in case of loss

---
+++ DB_script(how_lit).py
@@ -0,0 +1,24 @@
+from flask import Flask
+import json
+from flaskext.mysql import MySql
+
+#connect to the DB
+db1 = MS.connect(host="localhost",user="root",passwd="barry1")
+cursor = db1.cursor()
+sql = 'CREATE DATABASE IF NOT EXISTS how_lit_db'
+cursor.execute(sql)
+
+
+# create table
+cursor.execute("SET sql_notes = 0; ")
+cursor.execute("create table IF NOT EXISTS Artist Rankings (Artist varchar(70),Rank varchar(20));")
+cursor.execute("SET sql_notes = 1; ")
+
+#insert data
+cursor.execute("insert into test (Artist,Rank) values('The Weeknd','100')")
+
+# Commit changes in the database
+db.commit()
+
+# disconnect from server
+db.close()
